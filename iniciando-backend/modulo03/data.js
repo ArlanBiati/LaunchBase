69cd27cd1b44dee6323b85d3936e884159a88b7f
ArlanBiati <arlangustavo@hotmail.com>
2020-03-17 14:37:26 -0300

    Chore: Aplicando dados dinamicos

---
+++ iniciando-backend/modulo03/data.js
@@ -0,0 +1,43 @@
+module.exports = [
+  {
+    id: "IPLTUlRFxgo",
+    title: "Nenhuma PR passará sem padrões",
+    duration: "57 min",
+    price: "Free"
+  },
+
+  {
+    id: "IPLTUlRFxgo",
+    title: "Nenhuma PR passará sem padrões",
+    duration: "57 min",
+    price: "Free"
+  },
+
+  {
+    id: "IPLTUlRFxgo",
+    title: "Nenhuma PR passará sem padrões",
+    duration: "57 min",
+    price: "Free"
+  },
+
+  {
+    id: "IPLTUlRFxgo",
+    title: "Nenhuma PR passará sem padrões",
+    duration: "57 min",
+    price: "Free"
+  },
+
+  {
+    id: "IPLTUlRFxgo",
+    title: "Nenhuma PR passará sem padrões",
+    duration: "57 min",
+    price: "Free"
+  },
+
+  {
+    id: "IPLTUlRFxgo",
+    title: "Nenhuma PR passará sem padrões",
+    duration: "57 min",
+    price: "Free"
+  }
+]
--- iniciando-backend/modulo03/server.js
+++ iniciando-backend/modulo03/server.js
@@ -2,21 +2,40 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
+const videos = require("./data")
 
 server.use(express.static('public'))
 
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-  express: server
+  express: server,
+  autoescape: false
 })
 
 server.get("/", function (req, res) {
-  return res.render("about")
+
+  const about = {
+
+    avatar_url:"https://avatars0.githubusercontent.com/u/43690080?s=460&v=4",
+    name: "Arlan Biati",
+    role: "Desenvolvedor web FullStack",
+    description: "Programador Full-Stack, focado em trazer as    melhores interfaces para o cliente. Apaixonado pela linguagem Javascript e entusiasta de todo seu ecossistema",
+    links: [
+      {name: "Github", url: "https://github.com/ArlanBiati"},
+      {name: "Instagram", url: "https://www.instagram.com/arlangbiati/"},
+      {name: "Facebook", url: "https://www.facebook.com/arlan.biati"},
+      {name: "LinkedIn", url: "https://www.linkedin.com/in/arlan-biati-2b3512115/"}
+    ]
+
+  }
+
+  return res.render("about", { about })
 })
 
 server.get("/portfolio", function (req, res) {
-  return res.render("portfolio")
+
+  return res.render("portfolio", { items: videos })
 })
 
 server.listen(5000, function () {
