f7099ea6c12a105e7d6e6f80ea3dbc14a717cb8c
ArlanBiati <arlangustavo@hotmail.com>
2020-03-17 21:20:47 -0300

    Chore: Finalizado Dados Dinamicos

--- iniciando-backend/desafio-mod03/about.js
+++ iniciando-backend/desafio-mod03/about.js
@@ -1,8 +1,6 @@
-module.exports = [
+module.exports ={
 
-  {
-    logo_url: "./logo-rocketseat.svg",
+    logo_url: "/img/logo-rocketseat.svg",
     name: "Rocketseat",
     description: "Empresa especializada em ensinar jovens programadores a darem o próximo passo em suas carreiras. Atraves do ensino mão na massa, onde os alunos prendem praticando. Rocketseat é a empresa mais foda do planeta !"
-  }
-]
+}
--- iniciando-backend/desafio-mod03/courses.js
+++ iniciando-backend/desafio-mod03/courses.js
@@ -3,7 +3,7 @@ module.exports = [
   {
     id: "starter",
     description: "<strong>Torne-se um programador desejado</strong> no mercado com esses cursos gratuitos",
-    url_img: "./starter.svg",
+    url_img: "/img/starter.svg",
     module: "6 Módulos",
     price: "Free"
   },
@@ -11,7 +11,7 @@ module.exports = [
   {
     id: "launchbase",
     description: "<strong>Domine programação do zero</strong> e tenha acesso as melhores oportunidades do mercado",
-    url_img: "./launchbase.svg",
+    url_img: "/img/launchbase.svg",
     module: "17 Módulos",
     price: "Free"
   },
@@ -19,7 +19,7 @@ module.exports = [
   {
     id: "gostack",
     description: "<strong>Treinamento imersivo</strong> nas tecnologias mais modernas de desenvolvimento web e mobile",
-    url_img: "./gostack.svg",
+    url_img: "/img/gostack.svg",
     module: "20 Módulos",
     price: "Free"
   }
