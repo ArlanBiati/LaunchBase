1bd017a82f80131565577d7ff521a5b169928daa
ArlanBiati <arlangustavo@hotmail.com>
2020-04-22 08:47:44 -0300

    Fixed: Corrigindo erro de server

--- PrivateSchool/server.js
+++ PrivateSchool/server.js
@@ -4,6 +4,7 @@ const routes = require('./routes')
 
 const server = express()
 
+server.use(express.urlencoded({ extended: true }))
 server.use(express.static('public'))
 server.use(routes)
 
--- PrivateSchool/teachers.js
+++ PrivateSchool/teachers.js
@@ -14,7 +14,7 @@ exports.post = function (req, res) {
 
   birth = Date.parse(birth)
   const created_at = Date.now()
-  const id = Number(data.instructors.length + 1)
+  const id = Number(data.teachers.length + 1)
 
   data.teachers.push ({
     id,
