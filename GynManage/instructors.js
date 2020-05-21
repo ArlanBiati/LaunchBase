c07e154393e5bd471c7c40f2c7bc29b1d2992ecf
ArlanBiati <arlangustavo@hotmail.com>
2020-05-21 11:17:49 -0300

    Chore: Listando instrutores

--- GynManage/instructors.js
+++ GynManage/instructors.js
@@ -3,6 +3,10 @@ const data = require('./data.json')
 const { age, date } = require('./utils')
 const Intl = require('intl')
 
+exports.index = function(req, res) {
+  return res.render('instructors/index', { instructors: data.instructors })
+}
+
 
 // mostrar
 exports.show = function (req, res) {
@@ -105,7 +109,8 @@ exports.put = function (req, res) {
   const instructor = {
     ...foundInstructor,
     ...req.body,
-    birth: Date.parse(req.body.birth)
+    birth: Date.parse(req.body.birth),
+    id: Number(req.body.id)
   }
 
   data.instructors[index] = instructor
--- GynManage/routes.js
+++ GynManage/routes.js
@@ -6,9 +6,7 @@ routes.get('/', function(req, res) {
   return res.redirect('/instructors')
 })
 
-routes.get('/instructors', function(req, res) {
-  return res.render('instructors/index')
-})
+routes.get('/instructors', instructors.index)
 
 routes.get('/instructors/create', function(req, res) {
   return res.render('instructors/create')
